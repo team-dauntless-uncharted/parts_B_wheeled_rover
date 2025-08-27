a2ae8355edd1a9ab3c3cc00c09fce1b4ee112225
Reimanbow <reima@cps.im.dendai.ac.jp>
2025-08-27 16:27:13 +0900

    fix: Explore入ったらモータを回し続ける

--- src/Controller/States/ExploreState.cpp
+++ src/Controller/States/ExploreState.cpp
@@ -17,50 +17,56 @@ void ExploreState::onEnter() {
 
 void ExploreState::onUpdate() {
 	_ctx.getSerialWriter().log("Updating ExploreState");
+	static bool isMotorForward = false;
+	if (!isMotorForward) {
+		_ctx.getMotor().forward(255);
+		isMotorForward = true;
+	}
+	delay(500);
 
 	// 適当に移動
-	_ctx.getMotor().forward(150);
-	delay(random(500, 1500));
-	_ctx.getMotor().stop();
-	delay(100);	
+	// _ctx.getMotor().forward(150);
+	// delay(random(500, 1500));
+	// _ctx.getMotor().stop();
+	// delay(100);	
 
-	if (random(2) == 0) {
-		_ctx.getMotor().turnLeft(150);
-	} else {
-		_ctx.getMotor().turnRight(150);
-	}
-	delay(random(200, 500));
-	_ctx.getMotor().stop();
-	delay(100);
+	// if (random(2) == 0) {
+	// 	_ctx.getMotor().turnLeft(150);
+	// } else {
+	// 	_ctx.getMotor().turnRight(150);
+	// }
+	// delay(random(200, 500));
+	// _ctx.getMotor().stop();
+	// delay(100);
 
-	// 画像を撮影する
-    void* imgBuff = nullptr;
-    size_t imgSize = 0;
-    if (_ctx.getCamera().takePicture(&imgBuff, &imgSize)) {
-        _ctx.getSerialWriter().log("Save taken picture to SD card...");
-        _ctx.getSDLogger().saveJPEGImage(imgBuff, imgSize);
-    } else {
-		_ctx.getSerialWriter().log("Failed to take picture");
-    }
+	// // 画像を撮影する
+    // void* imgBuff = nullptr;
+    // size_t imgSize = 0;
+    // if (_ctx.getCamera().takePicture(&imgBuff, &imgSize)) {
+    //     _ctx.getSerialWriter().log("Save taken picture to SD card...");
+    //     _ctx.getSDLogger().saveJPEGImage(imgBuff, imgSize);
+    // } else {
+	// 	_ctx.getSerialWriter().log("Failed to take picture");
+    // }
 
-	double latitude = _ctx.getGnss().getLatitude();
-	double longitude = _ctx.getGnss().getLongitude();
+	// double latitude = _ctx.getGnss().getLatitude();
+	// double longitude = _ctx.getGnss().getLongitude();
 
-	if (fabs(latitude - _prevLatitude) < 0.000001 && fabs(longitude - _prevLongitude) < 0.000001) {
-		_sameCount++;
-	} else {
-		_sameCount = 0;
-	}
+	// if (fabs(latitude - _prevLatitude) < 0.000001 && fabs(longitude - _prevLongitude) < 0.000001) {
+	// 	_sameCount++;
+	// } else {
+	// 	_sameCount = 0;
+	// }
 
-	if (_sameCount >= SAME_LIMIT) {
-		// HELPING_STATE
-		_ctx.writeSystemLog("%lu: Can't Move. Changing to HELPING_STATE", millis());
-		_ctx.changeState(std::make_unique<HelpingState>(_ctx));
-		return;
-	}
+	// if (_sameCount >= SAME_LIMIT) {
+	// 	// HELPING_STATE
+	// 	_ctx.writeSystemLog("%lu: Can't Move. Changing to HELPING_STATE", millis());
+	// 	_ctx.changeState(std::make_unique<HelpingState>(_ctx));
+	// 	return;
+	// }
 
-	_prevLatitude = latitude;
-	_prevLongitude = longitude;
+	// _prevLatitude = latitude;
+	// _prevLongitude = longitude;
 }
 
 void ExploreState::onExit() {
