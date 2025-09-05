296d1052c236bce3bb2cce079066a04ddb39a8df
Reimanbow <reima@cps.im.dendai.ac.jp>
2025-09-05 22:45:01 +0900

    fix: いらないところの修正

--- src/Controller/States/EscapeState.cpp
+++ src/Controller/States/EscapeState.cpp
@@ -2,16 +2,11 @@
 #include "Controller/States/DetectionState.hpp"
 #include "Controller/CansatController.hpp"
 
-#include "Utils/GeoUtils/GeoUtils.hpp"
-
 void EscapeState::onEnter() {
 	_ctx.writeSystemLog("%lu: Entering EscapeState", millis());
 
 	// _ctx.getSpeaker().playState((int)State::ESCAPE);
 	_ctx.setLed((int)State::ESCAPE);
-
-	_startLatitude = _ctx.getGnss().getLatitude();
-	_startLongitude = _ctx.getGnss().getLongitude();
 
 	_startTime = millis();
 	_count = 0;
