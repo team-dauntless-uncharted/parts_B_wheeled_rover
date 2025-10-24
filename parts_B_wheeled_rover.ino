4155e215d4e09070f9828aa6c946c948ff2fde67
Reimanbow <reima@cps.im.dendai.ac.jp>
2025-10-25 00:56:08 +0900

    fix: メインプログラムのコメント追加

--- parts_B_wheeled_rover.ino
+++ parts_B_wheeled_rover.ino
@@ -1,14 +1,25 @@
+/**
+ * @file parts_B_wheeled_rover.ino
+ * @brief ARLISS 2025 TDU Team-Dauntless-Uncharted Bパーツメインプログラム
+ *
+ * @author TDU Team-Dauntless-Uncharted
+ * @date 2025
+ */
+
 #include <Arduino.h>
 #include "Controller/CansatController.hpp"
 
+/**
+ * @brief CanSat中央コントローラーのグローバルインスタンス
+ */
 CansatController cansat;
 
 void setup() {
     cansat.begin();
-    delay(2000);
+    delay(2000);  // システム安定化待ち
 }
 
 void loop() {
     cansat.update();
-    delay(200);
+    delay(200);  // 200ms周期（5Hz）
 }
