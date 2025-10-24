00139ecdcdc1d6bc1a22f879f6e355fd4c067580
Reimanbow <reima@cps.im.dendai.ac.jp>
2025-10-24 13:43:23 +0900

    add: CdS comment

--- src/Sensor/CdS/CdSSensor.cpp
+++ src/Sensor/CdS/CdSSensor.cpp
@@ -1,10 +1,18 @@
+/**
+ * @file CdSSensor.cpp
+ * @brief CdS光センサー制御クラスの実装
+ */
+
 #include <Arduino.h>
 #include "Sensor/CdS/CdSSensor.hpp"
 
 CdSSensor::CdSSensor(int pin) : _pin(pin) {
-    // ピン初期化など必要ならここで
+    // NOTE: アナログピンはpinMode()の設定不要
+    // analogRead()を呼ぶだけで自動的にアナログ入力として動作する
 }
 
 int CdSSensor::read() {
-	return analogRead(_pin);
+    // アナログ値（0-1023）を返す
+    // 値が大きい = 明るい、値が小さい = 暗い
+    return analogRead(_pin);
 }
--- src/Sensor/CdS/CdSSensor.hpp
+++ src/Sensor/CdS/CdSSensor.hpp
@@ -1,10 +1,35 @@
+/**
+ * @file CdSSensor.hpp
+ * @brief CdS光センサー（硫化カドミウムセル）の制御
+ */
+
 #pragma once
 
+/**
+ * @class CdSSensor
+ * @brief CdS光センサーで光量を測定するクラス
+ *
+ * アナログ入力ピンで光量を0-1023の値として読み取る
+ */
 class CdSSensor {
 public:
+	/**
+	 * @brief コンストラクタ
+	 * @param pin CdSセンサーが接続されているアナログピン番号
+	 *
+	 * 指定されたアナログピンからCdSセンサーの値を読み取れるよう初期化する
+	 */
 	CdSSensor(int pin);
+
+	/**
+	 * @brief 現在の光量を読み取る
+	 * @return 光量の値（0-1023）
+	 *
+	 * analogRead()を使用してセンサー値を取得する
+	 * 値が大きいほど明るく、小さいほど暗いことを示す
+	 */
 	int read();
 
 private:
-	int _pin;
+	int _pin;  ///< CdSセンサーが接続されているアナログピン番号
 };
