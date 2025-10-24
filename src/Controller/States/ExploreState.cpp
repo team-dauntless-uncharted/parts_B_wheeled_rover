db7bc3fc1efaccd440db2590ead3d8f66bff8afe
Reimanbow <reima@cps.im.dendai.ac.jp>
2025-10-25 00:03:34 +0900

    add: ExploreState comment

--- src/Controller/States/ExploreState.cpp
+++ src/Controller/States/ExploreState.cpp
@@ -1,3 +1,7 @@
+/**
+ * @file ExploreState.cpp
+ */
+
 #include "Controller/States/ExploreState.hpp"
 #include "Controller/CansatController.hpp"
 
@@ -7,6 +11,7 @@ void ExploreState::onEnter() {
 	// _ctx.getSpeaker().playState((int)State::EXPLORE);
 	_ctx.setLed((int)State::EXPLORE);
 
+	// PHOTO_MODEでカメラを初期化
 	if (!setPhotoMode()) {
 		_ctx.writeSystemLog("%lu: Failed to set explore mode", millis());
 	} else {
@@ -17,13 +22,15 @@ void ExploreState::onEnter() {
 void ExploreState::onUpdate() {
 	_ctx.getSerialWriter().log("Updating ExploreState");
 
+	// 蛇行前進（PWM150、2秒間、周期1ms）
 	// // 適当に移動
 	_ctx.getMotor().snakeForwardSmooth(150, 2000, 1);
 	// _ctx.getMotor().forward(150);
 	// delay(random(500, 1500));
 	// _ctx.getMotor().stop();
-	delay(100);	
+	delay(100);
 
+	// ランダムに左右旋回（200-500ms）
 	if (random(2) == 0) {
 		_ctx.getMotor().turnLeft(150);
 	} else {
@@ -33,7 +40,7 @@ void ExploreState::onUpdate() {
 	_ctx.getMotor().stop();
 	delay(100);
 
-	// 画像を撮影する
+	// 画像を撮影してSDカードに保存
     void* imgBuff = nullptr;
     size_t imgSize = 0;
     if (_ctx.getCamera().takePicture(&imgBuff, &imgSize)) {
@@ -46,6 +53,8 @@ void ExploreState::onUpdate() {
 
 void ExploreState::onExit() {
 	_ctx.writeSystemLog("%lu: Exiting ExploreState", millis());
+
+	// カメラの終了
 	if (_isInitCamera) {
 		endPhotoMode();
 	}
@@ -55,6 +64,7 @@ State ExploreState::getState() const {
 	return State::EXPLORE;
 }
 
+// PHOTO_MODEでカメラを初期化
 bool ExploreState::setPhotoMode() {
 	if (!_ctx.getCamera().begin(PHOTO_MODE)) {
 		return false;
@@ -68,6 +78,7 @@ bool ExploreState::setPhotoMode() {
 	return true;
 }
 
+// カメラの終了
 void ExploreState::endPhotoMode() {
 	_ctx.getCamera().end();
 }
